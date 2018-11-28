148c364a47d417ab0e5925fdc5f7b31bf2f96ac6
Shora <shorajafar@gmail.com>
2018-11-28 16:28:38 +0200

    third commit

--- src/components/Container/Container.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-class Container extends Component {
-  render() {
-    return <div>Content</div>;
-  }
-}
-
-export default Container;
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,12 +2,11 @@ import React from "react";
 
 import classes from "./Layout.module.css";
 import Aux from "../../hoc/Aux";
-import Container from "../Container/Container";
 
 const layout = props => (
   <Aux>
     <div>Header, footer, sideDrawer</div>
-    <Container className={classes.Content} />
+    <div className={classes.Content}>Content</div>
   </Aux>
 );
 
