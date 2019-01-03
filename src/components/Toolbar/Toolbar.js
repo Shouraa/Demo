c08a11fa02e58fe6340bb17eec5593eaf117e3af
Shora <shorajafar@gmail.com>
2019-01-03 18:28:10 +0200

    toolbar css

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -1,5 +1,24 @@
 import React from "react";
 
-const Toolbar = () => <div>Toolbar</div>;
+import classes from "./Toolbar.module.css";
+
+const Toolbar = () => (
+  <header className={classes.Toolbar}>
+    <div className={classes.Div}>
+      <a href="index.html" className={classes.Brand}>
+        LOGO
+      </a>
+    </div>
+
+    <nav className={classes.MainNav}>
+      <ul>
+        <li>Link</li>
+        <li className={classes.Login}>
+          <a href="#">SignUp/Login</a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+);
 
 export default Toolbar;
