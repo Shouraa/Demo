434a7c98899435ed80dfa96cab0ad78aefbc1a0d
Shouraa <shorajafar@gmail.com>
2019-03-18 22:00:48 +0200

    new name checked

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import classes from "./Footer.module.css";
 const Footer = () => (
   <div className={classes.Footer}>
     <h5>
-      2019 <span>Integrify</span>. All Rights Not Reserved.
+      2019 <span>Logo</span>. All Rights  Reserved.
     </h5>
   </div>
 );
