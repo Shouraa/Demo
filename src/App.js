5b2d5d2c4b1f57b05547a4212a7429e7a7690611
Shora <shorajafar@gmail.com>
2019-03-20 01:44:07 +0200

    Revert "02 feature form"

--- src/App.js
+++ src/App.js
@@ -1,63 +1,12 @@
 import React, { Component } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import "./App.css";
 
-import Layout from "./hoc/Layout/Layout";
-import SingleImageContainer from "./containers/SingleImageContainer/SingleImageContainer";
-import FrontPage from "./containers/FrontPage/FrontPage";
-import axios from "axios";
-
+import Layout from "./components/Layout/Layout";
 class App extends Component {
-  state = {
-    images: []
-  };
-
-  componentDidMount() {
-    axios
-      .get(
-        "https://api.unsplash.com/photos/?client_id=add2d2d52391495814a84b22b6089261f7133c5ac4b5a22017e11a4c7a6dc60b"
-      )
-      .then(response => {
-        const images = response.data.map(img => {
-          return {
-            ...img,
-            caption: "kireemamdardahanefelesastkakakkakakakkaka"
-          };
-        });
-        console.log(response.data);
-        this.setState({ images: images });
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }
-
-  filterHandler = id => {
-    return this.state.images.find(image => image.id === id);
-  };
-
   render() {
     return (
       <div>
-        <BrowserRouter>
-          <Layout>
-            <Switch>
-              <Route
-                path="/singleImage/:id"
-                render={props => (
-                  <SingleImageContainer
-                    image={this.filterHandler(props.match.params.id)}
-                    {...props}
-                  />
-                )}
-              />
-              <Route
-                path="/"
-                exact
-                render={props => <FrontPage {...this.state} />}
-              />
-            </Switch>
-          </Layout>
-        </BrowserRouter>
+        <Layout />
       </div>
     );
   }
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,5 @@
 import React from "react";
-import classes from "./Footer.module.css";
 
-const Footer = () => (
-  <div className={classes.Footer}>
-    <h5>
-      2019 <span>Logo</span>. All Rights Reserved.
-    </h5>
-  </div>
-);
+const Footer = () => <div>Footer</div>;
 
 export default Footer;
