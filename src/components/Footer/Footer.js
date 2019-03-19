b0884bb8ecfebe5c36cdf90213134d631b77dd86
Shouraa <shorajafar@gmail.com>
2019-03-20 01:36:18 +0200

    some content changes

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import classes from "./Footer.module.css";
 const Footer = () => (
   <div className={classes.Footer}>
     <h5>
-      2019 <span>Logo</span>. All Rights  Reserved.
+      2019 <span>Logo</span>. All Rights Reserved.
     </h5>
   </div>
 );
